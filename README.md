3fceab4d601e455b928e6c87e66d799d8b223933
workuser <not.indev@gmail.com>
2020-04-15 15:32:14 +0900

    update

---
+++ README.md
@@ -0,0 +1,17 @@
+<!-- ![](https://) -->
+
+# Title
+
+## Feature
+
+## Installation
+
+## Usage
+
+## License
+
+* Code: MIT License
+* Logos: CC BY-ND
+* This project includes open source software.
+
+## Disclaimer
---
+++ webpack.dev.js
@@ -0,0 +1,15 @@
+const merge = require('webpack-merge')
+const common = require('./webpack.common.js')
+
+const path = require('path')
+
+module.exports = merge(common, {
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    watchContentBase: true,
+    open: true
+  },
+  devtool: 'inline-source-map'
+}
+)
