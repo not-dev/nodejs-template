43c778d974c19ac76852f78396f0f48739364489
workuser <not.indev@gmail.com>
2020-04-16 10:08:10 +0900

    update js compresser

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,9 +2,20 @@ const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 
 const CopyPlugin = require('copy-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        extractComments: 'all',
+        terserOptions: {
+          compress: { drop_console: true }
+        }
+      })
+    ]
+  },
   plugins: [
     new CopyPlugin([
       { from: 'public' }
